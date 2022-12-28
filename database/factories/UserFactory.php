8e8eacb3dc87a2b69adfca38072a8f30da2ede96
unknown <1achrizky@gmail.com>
2022-12-28 22:23:46 +0700

    setup seeder factory(faker)

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,9 +17,18 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $fake_name = fake()->name();
+        $explo = explode(" ", $fake_name);
+        $username = Str::lower($explo[0].substr($explo[1], 0, 1)."123") ;
+        $email = $username."@gmail.com";
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            // 'name' => fake()->name(),
+            // 'username' => fake()->unique()->username(),
+            // 'email' => fake()->unique()->safeEmail(),
+            'name' => $fake_name,
+            'username' => $username,
+            'email' => $email,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +21,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Achmad Rizky',
+            'username' => '1achrizky',
+            'email' => '1achrizky@gmail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        User::factory(3)->create();
     }
 }
